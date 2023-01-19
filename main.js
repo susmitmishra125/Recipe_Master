129c040a0ac655c144171a31593043e7049136a9
Susmit Kumar Mishra <susmitmishra125@gmail.com>
2023-01-19 17:50:50 +0530

    typo fix

--- main.js
+++ main.js
@@ -5,20 +5,6 @@ fetch('data/data.json')
 	// dict_data contains the dictionary for the information in json file
 	});
 
-
-// create a function to return string
-function scale(s,multipler)
-{
-	let pattern = /^\s*([+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)\s*([a-zA-Z]+)?\s*$/;
-	let match = pattern.exec(s);
-	let quantity = match[1];
-	let unit = match[2];
-	// multiply quantity with multipler
-	quantity = quantity*multipler;
-	if(unit)
-		return quantity+unit;
-	return quantity;
-}
 document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
     let name = document.getElementById("name").value;
@@ -31,10 +17,6 @@ document.querySelector("form").addEventListener("submit", function(e) {
 		error.innerText = '';// to clear text
 		if (name in dict_data)
 		{
-			// item is present in the json file
-			// console.log("present");
-			// loop through the dict 
-			// console.log(dict_data[name]["ingredients"]);
 			for (let row of dict_data[name]["ingredients"]) {
 				let listItem = document.createElement("li");
 				listItem.innerText = row[0] + " : " + (row[1]*yield/dict_data[name]["yield"][0]).toFixed(2)+row[2];
